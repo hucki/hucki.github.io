3a7199216e053e1aa76386f80da5a080c6763099
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-07-04 16:47:09 +0200

    pwa optimized

---
+++ js/service-worker.js
@@ -0,0 +1,33 @@
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '../',
+  '../index.html',
+  '../css/style.css',
+  '../images/20160707_112632_P7070414.JPG'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
