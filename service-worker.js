808001daac688e807d774ff407fb7663cd7cafec
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-09-03 21:01:35 +0200

    fix(pwa): update service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,40 @@
-const cacheName = 'cache-v8';
+const cacheName = "cache-v9";
 const precacheResources = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/images/20160707_112632_P7070414.JPG',
-  '/images/icons/icon-72x72.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png'
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/images/grouptripper.jpg",
+  "/images/me.jpg",
+  "/images/me_half.jpg",
+  "/images/necto.jpg",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', event => {
-  console.log('Service worker install event!');
+self.addEventListener("install", (event) => {
+  console.log("Service worker install event!");
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(precacheResources);
-      })
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(precacheResources);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service worker activate event!');
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activate event!");
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request);
-      })
-    );
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch intercepted for:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
 });
