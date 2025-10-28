cb7d61de0341f38cb1375492e5558e640492d34d
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-10-28 23:48:30 +0100

    feat(Logo): new text logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,12 @@
+export const TextLogo = () => {
+  return (
+    <div className="flex flex-col items-center space-y-0">
+      <div className="font-display italic text-2xl sm:text-4xl text-outline-black text-white dark:text-outline-white dark:text-black">
+        STEFAN HUCKSCHLAG
+      </div>
+      <div className="font-display uppercase text-lg sm:text-2xl text-lime-500 -mt-2">
+        software engineering
+      </div>
+    </div>
+  );
+};
